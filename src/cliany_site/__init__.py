b10ab7ed8d22f806a966f332fd3e38a0dea76e66
pearjelly <pearjelly@gmail.com>
2026-05-21 12:24:59 +0800

    chore(release): bump version to 0.12.0

--- src/cliany_site/__init__.py
+++ src/cliany_site/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.12.0"
+
 from cliany_site.sdk import ClanySite, doctor, execute, explore, list_adapters, login
 
 __all__ = ["ClanySite", "explore", "execute", "login", "doctor", "list_adapters"]
